464f658617a37b260fa880a68254ed451f003acf
BernardOnuh <onuhbernard4@gmail.com>
2025-10-07 20:45:49 +0100

    update hero and index

--- web3nova-revamp/pages/index.js
+++ web3nova-revamp/pages/index.js
@@ -1,15 +1,15 @@
 import Image from "next/image";
 import { Geist, Geist_Mono } from "next/font/google";
-import { useRouter } from "next/router";
-import About from "../components/About";
-import HeroSection from "@/components/HeroSection";
-
+import Web3NovaHomepage from "@/components/Hero";
+import Navigation from "@/components/Hero/navbar";
+import Services from "./services";
 export default function Home() {
 
   return (
     <>
       <Navigation />
-      <HeroSection />
+      <Web3NovaHomepage />
+      <Services/>
       {/* Show About only on /about */}
     </>
   );
