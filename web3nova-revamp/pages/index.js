1a6213850c9c82f1020e6de9b6a15f4b307df5ec
Qasim Rokeeb <qasimrokeeb@gmail.com>
2025-10-07 18:36:01 +0100

    updated about pathway

--- web3nova-revamp/pages/index.js
+++ web3nova-revamp/pages/index.js
@@ -1,7 +1,7 @@
 import Image from "next/image";
 import { Geist, Geist_Mono } from "next/font/google";
 import { useRouter } from "next/router";
-import About from "../components/About/AboutPage";
+import About from "@/components/About/AboutPage";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -12,7 +12,7 @@ const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
   subsets: ["latin"],
 });
-import About from "../components/About/AboutPage";
+
 import HeroSection from "@/components/HeroSection";
 
 export default function Home() {
