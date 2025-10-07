5e4068c70c023a1be1644469b153bff1e4e18440
BernardOnuh <onuhbernard4@gmail.com>
2025-10-07 20:35:56 +0100

    Updated Home Page

--- web3nova-revamp/pages/index.js
+++ web3nova-revamp/pages/index.js
@@ -1,19 +1,18 @@
 import Image from "next/image";
 import { Geist, Geist_Mono } from "next/font/google";
-import { useRouter } from "next/router";
-import About from "../components/About";
-import HeroSection from "@/components/HeroSection";
+import HeroSection from "@/components/Hero";
+import Navigation from "@/components/Hero/navbar";
+import Services from "./services";
+
+
 
 export default function Home() {
-  const router = useRouter();
-
-  // Check if the route is "/about"
-  const isAboutPage = router.pathname === "/about";
 
   return (
     <>
+      <Navigation />
       <HeroSection />
-      {/* Show About only on /about */}
+      <Services />
     </>
   );
 }
