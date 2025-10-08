5ba4d738ad11cefb313e61867515091869baa9e8
BernardOnuh <onuhbernard4@gmail.com>
2025-10-08 19:46:47 +0100

    event

--- web3nova-revamp/pages/training.js
+++ web3nova-revamp/pages/training.js
@@ -1,10 +1,15 @@
 // pages/index.js
 import Training from '../components/Training/Training';
+import { Geist, Geist_Mono } from "next/font/google";
+import Navigation from '@/components/Hero/navbar';
+import Footer from '@/components/Footer';
 
 const HomePage = () => {
   return (
        <div className="bg-black">
+      <Navigation />
       <Training />
+      <Footer/>
       {/* Add other sections as needed */}
     </div>
   );
